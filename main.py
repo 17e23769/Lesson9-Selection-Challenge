2a3ed169e8d07995b8acae235dabd2de36546536
Henry Hurford <17e23769@marling.school>
2020-09-29 10:22:25 +0000

    Added second joke.

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import time
-Score = 0
+score = 0
 print ("Welcome To The Joke Game!")
 time.sleep(2)
 print ("Here Is The First Joke!")
 time.sleep(3)
 print ("\n")
 print ("what is pink and fluffy?")
-input
+a = str(input())
+if a == str("pink fluff"):
+  print ("You Are Correct!")
+  score = score + 1
+  print (score)
+else:
+  print ("You Are Wrong")
+  print (score)
+time.sleep(3)
+print("\n")
+print("Would You Like To See The Next Joke? please type yes or no")
+a = str(input())
+if a == str("no"):
+  print ("OK Goodbye")
+
+if a == str("yes"):
+  print("OK Here Is The Next Joke!")
+  time.sleep(3)
+  print("What is brown and sticky?")
+  a = str(input())
+  if a == str("a brown stick"):
+    print ("You Are Correct!")
+    score = score + 1
+    print (score)
+  else:
+    print ("you are wrong")
+    print (score)
