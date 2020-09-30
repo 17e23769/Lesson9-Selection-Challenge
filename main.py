03f6e8ea77c0a7968ce25e951d50642816c5116f
Henry Hurford <17e23769@marling.school>
2020-09-30 09:21:43 +0000

    Finished Joke game

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import time
 score = 0
 print ("Welcome To The Joke Game!")
-time.sleep(2)
+time.sleep(1)
 print ("Here Is The First Joke!")
-time.sleep(3)
+time.sleep(2)
 print ("\n")
 print ("what is pink and fluffy?")
 a = str(input())
@@ -14,7 +14,7 @@ if a == str("pink fluff"):
 else:
   print ("You Are Wrong")
   print (score)
-time.sleep(3)
+time.sleep(1)
 print("\n")
 print("Would You Like To See The Next Joke? please type yes or no")
 a = str(input())
@@ -23,7 +23,7 @@ if a == str("no"):
 
 if a == str("yes"):
   print("OK Here Is The Next Joke!")
-  time.sleep(3)
+  time.sleep(2)
   print("What is brown and sticky?")
   a = str(input())
   if a == str("a brown stick"):
@@ -33,3 +33,27 @@ if a == str("yes"):
   else:
     print ("you are wrong")
     print (score)
+  print ("\n")
+  print("Would You Like To See The Last Joke? please type yes or no")
+  a = str(input())
+
+  if a == str("no"):
+    print("OK Goodbye")
+  if a == str("yes"):
+    print ("OK Here Is The Last Joke!")
+    print ("\n")
+    time.sleep(2)
+    print ("What Is Black And White And Red All Over?")
+    a = str(input())
+    if a == str("a newspaper"):
+      print ("You Are Correct!")
+      score = score + 1
+      print ("this is your final score!")
+      print ("\n")
+      print (score)
+      print ("Thank You For Playing The Joke Game!")
+    else:
+      print ("You Are Wrong")
+      print ("this is your final score!")
+      print (score)
+      print ("Thank You For Playing The Joke Game!")
